c8aa80de8f0ae5e2c8f2866871fed4c5fd1933b7
Jean-Pierre Novak <jeanpierrenovak20@gmail.com>
2021-05-11 13:35:36 +0200

    Modified the styling of the SectionSegment component

--- src/components/SectionSegment.js
+++ src/components/SectionSegment.js
@@ -10,9 +10,9 @@ const SectionSegment = ({textPosition = 'left', image, text="text", header="head
 
     const renderText = (txt, head) => {
         return (
-            <div className={`${textClasses} width position-absolute mt-0 mt-md-5 pt-0 pt-md-5 h-100`}>
-                <h2 className="mx-auto mt-5 w-75">{head}</h2>
-                <p className="mx-auto w-75">{txt}</p>
+            <div className={`${textClasses} width position-absolute mt-0 mt-md-5 pt-0 pt-md-5 h-100 `}>
+                <h2 className="mx-auto mt-5 w-75 fs-1">{head}</h2>
+                <p className="mx-auto w-75 fs-4">{txt}</p>
             </div>
         )
     }
