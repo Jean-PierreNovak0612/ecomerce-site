3718af66d2a8fcd40eea2a2af08d2fd94d6e34ce
Jean-Pierre Novak <jeanpierrenovak20@gmail.com>
2021-05-11 15:03:52 +0200

    Fixed resoponsivnes

--- src/components/SectionSegment.js
+++ src/components/SectionSegment.js
@@ -11,9 +11,9 @@ const SectionSegment = ({textPosition = 'left', image, text="text", header="head
 
     const renderText = (txt, head, link) => {
         return (
-            <div className={`${textClasses} width position-absolute mt-0 mt-lg-5 pt-0 pt-md-5 h-100 `}>
-                <h2 className="ms-5 ps-5 mb-4 mb-lg-5 mt-5 w-75 fs-1">{head}</h2>
-                <p className="ms-5 ps-5 w-75 fs-4 mb-4 mb-lg-4 max-width">
+            <div className={`${textClasses} width position-absolute mt-0 mt-lg-5 pt-0 pt-md-5 `}>
+                <h2 className="ms-2 ms-md-5 ps-5 mb-4 mb-lg-5 mt-5 w-75 fs-1">{head}</h2>
+                <p className="ms-2 ms-md-5 ps-5 w-75 fs-4 mb-4 mb-lg-4 max-width">
                     {txt}
                     {renderButton(link)}
                 </p>
