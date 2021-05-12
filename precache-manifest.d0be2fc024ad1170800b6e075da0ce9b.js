9f9225a2f8a902cff0de07873ef1a98723b6ee00
Jean-Pierre Novak <jeanpierrenovak20@gmail.com>
2021-05-12 12:37:25 +0200

    Updates

---
+++ precache-manifest.d0be2fc024ad1170800b6e075da0ce9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d2a8bc507e49aa1fe76",
+    "url": "/ecomerce-site/static/css/main.d081c344.chunk.css"
+  },
+  {
+    "revision": "8d2a8bc507e49aa1fe76",
+    "url": "/ecomerce-site/static/js/main.0c945c39.chunk.js"
+  },
+  {
+    "revision": "1e2930d72bd9bd511643",
+    "url": "/ecomerce-site/static/js/runtime~main.64446f8d.js"
+  },
+  {
+    "revision": "e4aa5926e2f97c12cf7c",
+    "url": "/ecomerce-site/static/js/2.c0e22785.chunk.js"
+  },
+  {
+    "revision": "7929c8d9e30ba9d0757f765c46fb398c",
+    "url": "/ecomerce-site/index.html"
+  }
+];
